bbc55dc5d5bc20cfa621159d75276a7972f1658e
uncleman11 <darth.sidius92@gmail.com>
2025-09-07 01:54:20 +1200

    Simple script that shows all characters data in savefile.

---
+++ list-characters.php
@@ -0,0 +1,23 @@
+<?php
+	require_once("utils.php");
+
+    if (!isset($argv[1])) {
+		die("Usage: <original-SAVExxx.DAT> \nLists characters in savefile");
+	} elseif (!file_exists($argv[1])) {
+		die("Filename $argv[1] not found\n");
+	}
+
+	$save		= new \Disgaea\SaveFile($argv[1]);
+	$saveData	= $save->getSaveObject();
+
+	$characters			= $saveData->getChunk('characters');
+	print "Done\n";
+
+	foreach ($characters as $id => $character) {
+        print("\n");
+        print($character);
+        print("--------------------------------");
+        print("\n\n");
+	}
+
+	print "\n\n";
